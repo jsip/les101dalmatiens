c40ffa7f6a063a65d42819995cd55f9d88e233b6
jsip <jsimonroyer@gmail.com>
2022-05-15 14:33:59 -0400

    edit: UI changes

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,22 +1,52 @@
 /*global chrome*/
-import logo from './logo.svg';
+import logo from './manuvie-logo.png';
 import './App.css';
+import MaterialIcon from 'material-icons-react';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Woah it's a React Chrome extension...</p>
-        <a
-          className='App-link'
-          href='https://developer.chrome.com/docs/extensions/mv3/getstarted/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          View Extension Dev Docs
-        </a>
+        <h3 className="App-title">Les 101 Dalmatiens Lead Gen</h3>
       </header>
+      <main>
+        <section className="App-intro">
+          <p>Automating the process of LinkedIn lead generation for Manulife Asset Management.</p>
+          <div className="flex card">
+            <MaterialIcon icon="check" color="green" />
+            <p className="space-x">You currently are on the correct page to use this service.</p>
+          </div>
+        </section>
+        <section className="App-main">
+          <h5>Settings</h5>
+          <div className="Scrape-options-container">
+            <div className="Scrape-option-box">
+              <MaterialIcon icon="timer" size="medium"/>
+              <p className="bold no-padding-margin">Set a time limit</p>
+            </div>
+            <div className="Scrape-option-box">
+              <MaterialIcon icon="tune" size="medium"/>
+              <p className="bold no-padding-margin">Set a lead limit</p>
+            </div>
+            <div className="Scrape-option-box">
+              <MaterialIcon icon="all_inclusive" size="medium"/>
+              <p className="bold no-padding-margin">Set no limit</p>
+            </div>
+          </div>
+          <div className="Scrape-filename-input">
+            <h5>Please provide a name for the excel worksheet.</h5>
+            <div className="flex">
+              <input/>
+              <span className="light">.xlsx</span>
+            </div>
+          </div>
+          <hr />
+          <h5 className="light">Approximate count of prospects to be generated</h5>
+          <h3>1,756</h3>
+          <button className="Scrape-cta">Run Les 101 Dalmatiens</button>
+        </section>
+      </main>
     </div>
   );
 }
